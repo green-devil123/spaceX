da6e0b67112ca688045b1b4e9fa2110d99457209
Tarun Agarwal <agarwal.tarun90@gmail.com>
2020-09-08 14:02:54 +0530

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { SpaceXService } from './services/space-x.service';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
